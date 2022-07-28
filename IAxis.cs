19b3115a5bc5a1d1b7fac4e90d800522b280df29
AmRUknA <109937491+AmRUknA@users.noreply.github.com>
2022-07-28 17:07:41 +0700

    Add files via upload

---
+++ IAxis.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Motion_Foca
+{
+    public interface IAxis
+    {
+        int AxisID { get; }
+        decimal Factor { get; set; }
+         int OriginDirection { get; set; }
+        decimal PositionAfterHome { get; set; }
+        decimal CurrentPosition { get; }
+        decimal CurrentVelocity { get; }
+        bool Inposition { get; }
+        bool Emergency { get; }
+        bool OriginPosition { get; }
+        
+        void MoveAbsolute(decimal Position, decimal Velocity);
+        void MoveAbsolute(decimal Position, decimal Velocity, decimal Acc);
+        void MoveAbsolute(decimal Position, decimal Velocity, decimal Acc, decimal Dec);
+        void MoveRelative(decimal Distance, decimal Velocity);
+        void MoveRelative(decimal Distance, decimal Velocity, decimal Acc);
+        void MoveRelative(decimal Distance, decimal Velocity, decimal Acc, decimal Dec);
+        void StopMove();
+        void SearchOrigin();
+        void SetPosition(decimal Position);
+        void Jog(decimal Velocity, int Direction);
+
+
+    }
+
+    
+}
